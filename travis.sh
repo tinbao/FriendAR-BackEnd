c4c8f2effacf9ff41af9f495820c0dc3d8a4d2d0
James Stone <jamesmstone@users.noreply.github.com>
2017-08-21 14:14:14 +1000

    #29 - Add PMD source code analysis, to CI test

--- travis.sh
+++ travis.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 set -ex
-docker run -it --rm --user="$(id -u):$(id -g)" -v "$(pwd)/friendar-rest-api":/usr/src/mymaven -w /usr/src/mymaven maven:alpine mvn clean test
+mvn(){
+	docker run -it --rm --user="$(id -u):$(id -g)" -v "$(pwd)/friendar-rest-api":/usr/src/mymaven -w /usr/src/mymaven maven:alpine mvn "$@"
+}
+mvn pmd:check
+mvn pmd:cpd-check
+mvn clean test
