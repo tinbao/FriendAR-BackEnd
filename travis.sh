b96be9abe8ac22ae4a9628375a4a0457cd7c6ec3
James Stone <jamesmstone@users.noreply.github.com>
2017-08-21 15:12:06 +1000

    change to use batch mode and remove overly verbose download logs from ci env

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 mvn(){
-	docker run -it --rm --user="$(id -u):$(id -g)" -v "$(pwd)/friendar-rest-api":/usr/src/mymaven -w /usr/src/mymaven maven:alpine mvn "$@"
+	docker run -it --rm --user="$(id -u):$(id -g)" -v "$(pwd)/friendar-rest-api":/usr/src/mymaven -w /usr/src/mymaven maven:alpine mvn -B "$@" | grep -v 'Download.* http'
 }
 mvn pmd:check
 mvn pmd:cpd-check
