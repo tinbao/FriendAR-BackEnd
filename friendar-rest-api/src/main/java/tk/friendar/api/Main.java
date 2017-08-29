d84d6c8634552e5e62694e6f5026d13231b83926
James Stone <jamesmstone@users.noreply.github.com>
2017-08-30 01:19:52 +1000

    connect docker with rest api

--- friendar-rest-api/src/main/java/tk/friendar/api/Main.java
+++ friendar-rest-api/src/main/java/tk/friendar/api/Main.java
@@ -13,7 +13,7 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    public static final String BASE_URI = "http://0.0.0.0:80/1/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -38,8 +38,8 @@ public class Main {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+//        System.in.read();
+  //      server.stop();
     }
 }
 
