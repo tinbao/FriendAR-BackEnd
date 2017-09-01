d61ac2d7239974a1a21bd51d4404a089b1281bc1
habibiM96 <habibia@student.unimelb.edu.au>
2017-09-01 16:25:08 +1000

    Add init.sql script to docker database image & fix primary keys' types & other SQL syntax errors such as reserved words

--- DB/init.sql
+++ DB/init.sql
@@ -1,48 +1,44 @@
-CREATE TABLE IF NOT EXISTS User(
-    userID  TEXT PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS Users(
+    UserID  SERIAL PRIMARY KEY NOT NULL,
     fullName TEXT,
-    username TEXT NOT NULL,
-    userPassword TEXT NOT NULL,
+    Usersname TEXT NOT NULL,
+    UsersPassword TEXT NOT NULL,
     salt TEXT NOT NULL,
     email TEXT NOT NULL,
-    lat DECIMAL,
-    long DECIMAL,
+    latitude DECIMAL,
+    longtitude DECIMAL,
     locationLastUpdated TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Place(
-    placeID TEXT PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS Places(
+    placeID SERIAL PRIMARY KEY NOT NULL,
     placeName TEXT,
-    lat DECIMAL,
-    long DECIMAL
+    latitude DECIMAL,
+    longtitude DECIMAL
 );
 
-CREATE TABLE IF NOT EXISTS Meeting(
-    meetingID TEXT NOT NULL,
-    placeID TEXT REFERENCES Place (placeID) NOT NULL,
+CREATE TABLE IF NOT EXISTS Meetings(
+    meetingID SERIAL PRIMARY KEY NOT NULL,
+    placeID SERIAL REFERENCES Places (placeID) NOT NULL,
     meetingName TEXT,
     timeDate TIMESTAMP NOT NULL
-    PRIMARY KEY (meetingID, placeID)
 );
 
-CREATE TABLE IF NOT EXISTS MeetingUser(
-    meetingUserID TEXT NOT NULL,
-    meetingID TEXT REFERENCES Meeting (meetingID) NOT NULL,
-    userID TEXT REFERENCES [User] (userID) NOT NULL,
-    PRIMARY KEY (meetingUserID, meetingID, userID)
+CREATE TABLE IF NOT EXISTS MeetingUsers(
+    meetingUserID SERIAL PRIMARY KEY NOT NULL,
+    meetingID SERIAL REFERENCES Meetings (meetingID) NOT NULL,
+    UserID SERIAL REFERENCES Users (UserID) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Friendship(
-    friendshipID TEXT NOT NULL,
-    userA_ID TEXT REFERENCES [User] (userID) NOT NULL,
-    userB_ID TEXT REFERENCES [User] (userID) NOT NULL,
-    PRIMARY KEY(friendshipID, userA_ID, userB_ID)
+CREATE TABLE IF NOT EXISTS Friendships(
+    friendshipID SERIAL PRIMARY KEY NOT NULL,
+    UserA_ID SERIAL REFERENCES Users (UserID) NOT NULL,
+    UserB_ID SERIAL REFERENCES Users (UserID) NOT NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS Mesage(
-    messageID TEXT NOT NULL,
-    meetingID TEXT REFERENCES [User] (userID) NOT NULL,
-    userID TEXT REFERENCES [User] (userID) NOT NULL,
-    PRIMARY KEY(friendshipID, meetingID, userID)
+CREATE TABLE IF NOT EXISTS Messages(
+    messageID SERIAL PRIMARY KEY NOT NULL,
+    meetingID SERIAL REFERENCES Users (UserID) NOT NULL,
+    UserID SERIAL REFERENCES Users (UserID) NOT NULL
 );
