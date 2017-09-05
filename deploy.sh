f57dba96efe6c00b2888ad34483248c094bd10e1
James Stone <jamesmstone@users.noreply.github.com>
2017-09-05 22:46:42 +1000

    add debug info

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,7 @@ sudo apt-get -qq install --no-install-recommends -y openssh-server
 ssh -V
 ssh -i ssh_key -o StrictHostKeyChecking=no -fnNT -L $(pwd)/docker.sock:/var/run/docker.sock ubuntu@01.server.friendar.tk.
 export DOCKER_HOST=$(pwd)/docker.sock
-docker-compose build --pull --no-cache --force-rm && docker-compose up --remove-orphans -d
+docker info
+docker ps
+docker images
+docker-compose -H $(pwd)/docker.sock  build --pull --no-cache --force-rm && docker-compose up --remove-orphans -d
