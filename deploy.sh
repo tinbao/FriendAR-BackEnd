99c31625e91af6b4029bda851fd1a55dfe1cc7f3
James Stone <jamesmstone@users.noreply.github.com>
2017-09-05 18:48:59 +1000

    check ssh version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#set -e # Exit with nonzero exit code if anything fails
+set -e # Exit with nonzero exit code if anything fails
+set -o
+
+ssh -V
 
 ssh -i ssh_key -o StrictHostKeyChecking=no -fnNT -L $(pwd)/docker.sock:/var/run/docker.sock
 export DOCKER_HOST=$(pwd)/docker.sock
