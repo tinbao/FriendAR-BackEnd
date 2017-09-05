b488ae1715dfca92fa15279fe586abd31a7833fe
James Stone <jamesmstone@users.noreply.github.com>
2017-09-05 22:24:29 +1000

    fix missing server address

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ set -e # Exit with nonzero exit code if anything fails
 set -o
 
 ssh -V
-sudo apt-add-repository 'deb http://archive.ubuntu.com/ubuntu yakkety main universe multiverse' && sudo apt-get -qq update -y && sudo apt-get -qq install openssh-server -y>
+sudo apt-add-repository 'deb http://archive.ubuntu.com/ubuntu yakkety main universe multiverse' && sudo apt-get -qq update -y && sudo apt-get -qq install openssh-server -y
 ssh -V
-ssh -i ssh_key -o StrictHostKeyChecking=no -fnNT -L $(pwd)/docker.sock:/var/run/docker.sock
+ssh -i ssh_key -o StrictHostKeyChecking=no -fnNT -L $(pwd)/docker.sock:/var/run/docker.sock ubuntu@01.server.friendar.tk.
 export DOCKER_HOST=$(pwd)/docker.sock
 docker-compose build --pull --no-cache --force-rm && docker-compose up --remove-orphans -d
