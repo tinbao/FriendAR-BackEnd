0774394a43ec7630d7a2234ac3213824f3f58f16
James Stone <jamesmstone@users.noreply.github.com>
2017-09-05 20:34:24 +1000

    Make install less verbose, now ssh install is working.

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 set -o
 
 ssh -V
-sudo apt-add-repository 'deb http://archive.ubuntu.com/ubuntu yakkety main universe multiverse'
-sudo apt-get update -y 
-sudo apt-get install openssh-server -y 
+sudo apt-add-repository 'deb http://archive.ubuntu.com/ubuntu yakkety main universe multiverse' && sudo apt-get -qq update -y && sudo apt-get -qq install openssh-server -y>
 ssh -V
 ssh -i ssh_key -o StrictHostKeyChecking=no -fnNT -L $(pwd)/docker.sock:/var/run/docker.sock
 export DOCKER_HOST=$(pwd)/docker.sock
