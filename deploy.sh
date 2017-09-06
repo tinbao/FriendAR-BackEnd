53abd377d92d7a6e5e79ced21b9c394055e2be11
James Stone <jamesmstone@users.noreply.github.com>
2017-09-06 11:20:50 +1000

    Revert "update ssh version"
    This reverts commit 8bf1eb16930944ddc1e0425e6b037aea3f8aeaf3.

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 set -o
 
 ssh -V
-apt-get update -y -qq && apt-get install openssh-server -y -qq
-ssh -V
+
 ssh -i ssh_key -o StrictHostKeyChecking=no -fnNT -L $(pwd)/docker.sock:/var/run/docker.sock
 export DOCKER_HOST=$(pwd)/docker.sock
 docker-compose build --pull --no-cache --force-rm && docker-compose up --remove-orphans -d
