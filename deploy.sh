a4dc0d17e4bb0075bb5d2279e33e834b1c4a72c9
James Stone <jamesmstone@users.noreply.github.com>
2017-09-06 11:20:27 +1000

    Revert "update openssh"
    This reverts commit 4ea724e7e26572697f64598020c01e4ffd45f422.

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ set -e # Exit with nonzero exit code if anything fails
 set -o
 
 ssh -V
-sudo apt-add-repository 'deb http://archive.ubuntu.com/ubuntu yakkety main universe multiverse'
-sudo apt-get -qq update -y
-sudo apt-get -qq install --no-install-recommends -y openssh-server
-ssh -V
+#sudo apt-add-repository 'deb http://archive.ubuntu.com/ubuntu yakkety main universe multiverse'
+#sudo apt-get -qq update -y
+#sudo apt-get -qq install --no-install-recommends -y openssh-server
+#ssh -V
 ssh -i ssh_key -o StrictHostKeyChecking=no -fnNT -L $(pwd)/docker.sock:/var/run/docker.sock ubuntu@01.server.friendar.tk.
 export DOCKER_HOST=$(pwd)/docker.sock
 docker info
