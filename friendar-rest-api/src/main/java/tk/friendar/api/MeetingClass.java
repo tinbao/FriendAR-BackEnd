9c25789da8c0b836d7ad6ee7df202281e06cc302
habibiM96 <habibia@student.unimelb.edu.au>
2017-09-08 11:38:06 +1000

    removed the unused imports

--- friendar-rest-api/src/main/java/tk/friendar/api/MeetingClass.java
+++ friendar-rest-api/src/main/java/tk/friendar/api/MeetingClass.java
@@ -1,9 +1,5 @@
 package tk.friendar.api;
 
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
-
 import java.io.IOException;
 import java.net.URI;
 import java.sql.Timestamp;
