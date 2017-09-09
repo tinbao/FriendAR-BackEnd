97ec20d0acce69b1c432ce424cc95711dc2ebb12
James Stone <jamesmstone@users.noreply.github.com>
2017-09-09 10:10:12 +1000

    remove false info on how to stop server

--- friendar-rest-api/src/main/java/tk/friendar/api/Main.java
+++ friendar-rest-api/src/main/java/tk/friendar/api/Main.java
@@ -38,7 +38,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+                + "%sapplication.wadl\n", BASE_URI));
         // register shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             @Override
