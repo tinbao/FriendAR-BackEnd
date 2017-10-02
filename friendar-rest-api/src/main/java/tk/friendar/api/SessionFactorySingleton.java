39a5a78d78368f7b71d44d2245501c57e05bef16
James Stone <jamesmstone@users.noreply.github.com>
2017-10-02 20:45:39 +1100

    remove signleton as causes server to crash (properly)

--- friendar-rest-api/src/main/java/tk/friendar/api/SessionFactorySingleton.java
+++ friendar-rest-api/src/main/java/tk/friendar/api/SessionFactorySingleton.java
@@ -4,15 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class SessionFactorySingleton {
-
-    private static SessionFactory instance = null;
-
-    private SessionFactorySingleton() {
-        // Exists only to defeat instantiation.
-    }
-
     public static SessionFactory getInstance() {
-        return new Configuration().configure().buildSessionFactory();
-        // not a singleton as this gets closed, after a request.
+        return new Configuration().configure().buildSessionFactory(); // not a singleton as this gets closed, after a request.
     }
 }
