0a47609f683a04007f44da424c05360d3afa0719
James Stone <jamesmstone@users.noreply.github.com>
2017-10-11 21:59:10 +1100

    make test running safer and cleaner for when running localy, add database to tests for #72

--- tests.sh
+++ tests.sh
@@ -4,8 +4,8 @@ set -o pipefail
 mvn(){
 	docker run -it --rm -v "$(pwd)/friendar-rest-api":/usr/src/mymaven -w /usr/src/mymaven maven:alpine mvn -B "$@" | grep -v 'Download.* http'
 }
-
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d db
+docker-compose down # ensure we are fresh and clean.
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build -d db
 mvn clean test
 docker-compose down
 docker-compose build --force-rm --no-cache --pull | grep -v 'Download.* http'
