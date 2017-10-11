2f3befe18240cecf76c8f7d39480c1086c97e9d8
James Stone <jamesmstone@users.noreply.github.com>
2017-10-11 21:40:22 +1100

    add database to tests for #72

--- tests.sh
+++ tests.sh
@@ -5,7 +5,9 @@ mvn(){
 	docker run -it --rm -v "$(pwd)/friendar-rest-api":/usr/src/mymaven -w /usr/src/mymaven maven:alpine mvn -B "$@" | grep -v 'Download.* http'
 }
 
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d db
 mvn clean test
+docker-compose down
 docker-compose build --force-rm --no-cache --pull | grep -v 'Download.* http'
 mvn pmd:check
 mvn pmd:cpd-check
