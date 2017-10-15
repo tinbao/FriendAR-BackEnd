08f21531713fbf62169f3f43617b546e02455279
habibiM96 <habibia@student.unimelb.edu.au>
2017-10-15 22:58:49 +1100

    made tests for Friendship endpoint and meetingUser endpoint, also made the meetingUser Endpoint

---
+++ friendar-rest-api/src/main/java/tk/friendar/api/MeetingUserEndpoint.java
@@ -0,0 +1,97 @@
+package tk.friendar.api;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Root resource (exposed at "meetingusers" path)
+ */
+@Path("meetingusers")
+public class MeetingUserEndpoint {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a application/json response.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String get() throws JSONException {
+        try (Session session = SessionFactorySingleton.getInstance().openSession()) {
+
+            List<MeetingUserDB> meetingUsersDB = session.createCriteria(MeetingUserDB.class).list();
+            JSONObject json = new JSONObject();
+            for (MeetingUserDB meetingUser : meetingUsersDB) {
+                json.append("meetingUsers: ", meetingUser.toJson(true));
+            }
+
+            return json.toString();
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String create(String meetinguserJson) throws JSONException {
+        try {
+
+            JSONObject json = new JSONObject(meetinguserJson);
+            MeetingUserDB meetingUser = new MeetingUserDB();
+            boolean update = false;
+
+            meetingUser.setMeetingID(json.getInt("meetingID"));
+            meetingUser.setUserID(json.getInt("userID"));
+
+            try (Session session = SessionFactorySingleton.getInstance().openSession()) {
+                session.beginTransaction();
+                session.save(meetingUser);
+                String response = meetingUser.toJson(false).toString();
+                session.getTransaction().commit();
+                return response;
+            }
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
+
+    @Path("{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String get(@PathParam("id") String id) {
+        try (Session session = SessionFactorySingleton.getInstance().openSession()) {
+            try {
+                return session.get(MeetingUserDB.class, Integer.valueOf(id)).toJson(false).toString();
+            } catch (Exception e) {
+                return e.toString();
+            }
+        }
+    }
+
+    @Path("{id}")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String delete(@PathParam("id") String id) {
+        try (Session session = SessionFactorySingleton.getInstance().openSession()) {
+            try {
+                session.beginTransaction();
+                MeetingUserDB meetingUser = session.get(MeetingUserDB.class, Integer.valueOf(id));
+                session.delete(meetingUser);
+                session.getTransaction().commit();
+                return meetingUser.toJson(false).toString();
+            } catch (HibernateException | JSONException e) {
+                return e.toString();
+            }
+        }
+    }
+}
---
+++ friendar-rest-api/src/test/java/tk/friendar/api/MeetingUserEndpointTest.java
@@ -0,0 +1,144 @@
+package tk.friendar.api;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertNotNull;
+
+public class MeetingUserEndpointTest {
+
+    private HttpServer server;
+    private WebTarget target;
+
+    @Before
+    public void setUp() throws Exception {
+        // start the server
+        server = Main.startServer();
+        // create the client
+        HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder()
+                .nonPreemptive()
+                .credentials("Luca@gmail.com", "harris")
+                .build();
+
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.register(feature);
+        Client client = ClientBuilder.newClient(clientConfig);
+        target = client.target(Main.BASE_URI);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.shutdownNow();
+    }
+
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+
+
+    /******************************* @POST tests *******************************/
+    @Test
+    public void testPOSTCompleteMeetingUsers() throws Exception {
+        //Complete data with lat or long
+        String test = "{\"meetingID\": 1, \"userID\": 1}";
+        Response msg = target.path("meetingusers").request().accept(MediaType.APPLICATION_JSON).post(Entity.json(test), Response.class);
+        String output = msg.readEntity(String.class);
+        assertNotNull(msg);
+        assertNotNull(output);
+        assert (output.toLowerCase().indexOf("meetingUserID".toLowerCase()) != -1);
+
+    }
+
+    @Test
+    public void testPOSTCompleteMeetingUsersAnother() throws Exception {
+        //Complete data with lat or long
+        String test = "{\"meetingID\": 1, \"userID\": 3}";
+        Response msg = target.path("meetingusers").request().accept(MediaType.APPLICATION_JSON).post(Entity.json(test), Response.class);
+        String output = msg.readEntity(String.class);
+        assertNotNull(msg);
+        assertNotNull(output);
+        assert (output.toLowerCase().indexOf("meetingUserID".toLowerCase()) != -1);
+
+
+    }
+
+    @Test
+    public void testPOSTwithoutUserID() throws Exception {
+        //Complete data with lat or long
+        String test = "{\"meetingID\": 45}";
+        Response msg = target.path("meetingusers").request().accept(MediaType.APPLICATION_JSON).post(Entity.json(test), Response.class);
+        String output = msg.readEntity(String.class);
+        assertNotNull(msg);
+        assertNotNull(output);
+        assert (output.toLowerCase().indexOf("org.json.JSONException: JSONObject[\"userID\"] not found.".toLowerCase()) != -1);
+    }
+
+
+    /******************************* @GET tests *******************************/
+    @Test
+    public void testGetAllMeetingUsers() {
+        //getting entire list of users in form of a string
+        String allMeetingUsers = target.path("meetingusers").request().get(String.class);
+        assertNotNull(allMeetingUsers);
+        assert allMeetingUsers.toLowerCase().contains("meetingUsers: ".toLowerCase());
+    }
+
+    @Test
+    public void testGetParticularMeetingUser() {
+        //getting a specific user in form of a string
+        String meetingUser = target.path("meetingusers").path("1").request().get(String.class);
+        assertNotNull(meetingUser);
+        assert (meetingUser.toLowerCase().indexOf("meetingUserID".toLowerCase()) != -1);
+    }
+
+    @Test
+    public void testGetParticularMeetingUser_empty() {
+        //getting a specific user that doesn't exist
+        try {
+            //getting a specific user in form of a string
+            String invalidMeetingUser = target.path("meetingusers").path("45").request().get(String.class);
+            assertNotNull(invalidMeetingUser);
+            assert (invalidMeetingUser.toLowerCase().indexOf("java.lang.NullPointerException".toLowerCase()) != -1);
+        } catch (Exception e) {
+
+        }
+
+    }
+
+
+    /******************************* @Delete tests *******************************/
+    @Test
+    public void testAValidDelete() throws Exception {
+        Response msg;
+        String output;
+        //Deleting an existing place
+        msg = target.path("meetingusers").path("2").request().accept(MediaType.APPLICATION_JSON).delete();
+        output = msg.readEntity(String.class);
+        assertNotNull(msg);
+        assertNotNull(output);
+        assert (output.toLowerCase().indexOf("meetingUserID".toLowerCase()) != -1);
+    }
+
+    @Test
+    public void testAnInvalidDelete() throws Exception {
+        Response msg;
+        String output;
+        //Deleting an existing place
+        msg = target.path("meetingusers").path("45").request().accept(MediaType.APPLICATION_JSON).delete();
+        output = msg.readEntity(String.class);
+        assertNotNull(msg);
+        assertNotNull(output);
+        assert (output.equalsIgnoreCase("<html><head><title>Grizzly 2.4.0</title><style><!--div.header {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#003300;font-size:22px;-moz-border-radius-topleft: 10px;border-top-left-radius: 10px;-moz-border-radius-topright: 10px;border-top-right-radius: 10px;padding-left: 5px}div.body {font-family:Tahoma,Arial,sans-serif;color:black;background-color:#FFFFCC;font-size:16px;padding-top:10px;padding-bottom:10px;padding-left:10px}div.footer {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#666633;font-size:14px;-moz-border-radius-bottomleft: 10px;border-bottom-left-radius: 10px;-moz-border-radius-bottomright: 10px;border-bottom-right-radius: 10px;padding-left: 5px}BODY {font-family:Tahoma,Arial,sans-serif;color:black;background-color:white;}B {font-family:Tahoma,Arial,sans-serif;color:black;}A {color : black;}HR {color : #999966;}--></style> </head><body><div class=\"header\">Request failed.</div><div class=\"body\">Request failed.</div><div class=\"footer\">Grizzly 2.4.0</div></body></html>"));
+    }
+}
