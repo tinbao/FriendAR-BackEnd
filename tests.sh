ca95d0fdc63e224a43ded16569a726bf1c81d180
James Stone <jamesmstone@users.noreply.github.com>
2017-10-16 19:30:13 +1100

    improve running of tests in docker

--- tests.sh
+++ tests.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -ex
 set -o pipefail
-mvn(){
-	docker run -it --rm -v "$(pwd)/friendar-rest-api":/usr/src/mymaven -w /usr/src/mymaven maven:alpine mvn -B "$@" | grep -v 'Download.* http'
-}
 
 docker-compose down # ensure we are fresh and clean.
-docker-compose -f docker-compose.tests.yml up --build
-
-# mvn clean test
-# docker-compose down
-# docker-compose build --force-rm --no-cache --pull | grep -v 'Download.* http'
-# mvn pmd:check
-# mvn pmd:cpd-check
+docker-compose build --force-rm --no-cache --pull | grep -v 'Download.* http'
+docker-compose run web sh -c "mvn clean test && mvn pmd:cpd-check && mvn pmd:check"
