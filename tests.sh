8696165331f108e9299cfe37a7bdb61fc967c905
James Stone <jamesmstone@users.noreply.github.com>
2017-10-17 01:58:12 +1100

    attempt run tests one at a time

--- tests.sh
+++ tests.sh
@@ -4,4 +4,11 @@ set -o pipefail
 
 docker-compose down # ensure we are fresh and clean.
 docker-compose build --force-rm --no-cache --pull | grep -v 'Download.* http'
-docker-compose run web sh -c "mvn clean test && mvn pmd:cpd-check && mvn pmd:check"
+docker-compose run web sh -c "mvn pmd:cpd-check && mvn pmd:check"
+
+docker-compose run web sh -c "mvn clean test -Dtest=ChatEndpointTest"
+docker-compose run web sh -c "mvn clean test -Dtest=FriendshipEndpointTest"
+docker-compose run web sh -c "mvn clean test -Dtest=MeetingEndpointTest"
+docker-compose run web sh -c "mvn clean test -Dtest=MeetingUserEndpointTest"
+docker-compose run web sh -c "mvn clean test -Dtest=PlacesEndpointTest"
+docker-compose run web sh -c "mvn clean test -Dtest=UsersEndpointTest"
