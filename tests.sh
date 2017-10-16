d474b520c72bc6678f10f46954b2bfcab0dcbbac
James Stone <jamesmstone@users.noreply.github.com>
2017-10-16 19:27:08 +1100

    phase 1 - totaly move tests into docker

--- tests.sh
+++ tests.sh
@@ -5,17 +5,11 @@ mvn(){
 	docker run -it --rm -v "$(pwd)/friendar-rest-api":/usr/src/mymaven -w /usr/src/mymaven maven:alpine mvn -B "$@" | grep -v 'Download.* http'
 }
 
-if [ "$CI" == "true" ]
-then
-    echo "CI only mode"
-    sudo kill -9 $(sudo lsof -t -i:5432)
-    echo "end CI only mode"
-fi
-
 docker-compose down # ensure we are fresh and clean.
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build -d db
-mvn clean test
-docker-compose down
-docker-compose build --force-rm --no-cache --pull | grep -v 'Download.* http'
-mvn pmd:check
-mvn pmd:cpd-check
+docker-compose -f docker-compose.tests.yml up --build
+
+# mvn clean test
+# docker-compose down
+# docker-compose build --force-rm --no-cache --pull | grep -v 'Download.* http'
+# mvn pmd:check
+# mvn pmd:cpd-check
