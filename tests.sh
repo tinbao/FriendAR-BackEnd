bb3fcada5773837f67ad04f2db004f03f0e4f9c8
James Stone <jamesmstone@users.noreply.github.com>
2017-10-20 15:56:38 +1100

    remove grep from test script as we dont want to use its status code

--- tests.sh
+++ tests.sh
@@ -3,27 +3,27 @@ set -ex
 set -o pipefail
 
 docker-compose down # ensure we are fresh and clean.
-docker-compose build --force-rm --no-cache --pull | grep -v 'Download.* http'
+docker-compose build --force-rm --no-cache --pull
+
+docker-compose run -T web sh -c "mvn pmd:cpd-check && mvn pmd:check"
 
 docker-compose down # ensure we are fresh and clean.
 docker-compose rm -v
-docker-compose run web sh -c "mvn clean test -Dtest=ChatEndpointTest"  2>/dev/null | grep -v 'Download.* http'
+docker-compose run web sh -c "mvn clean test -Dtest=ChatEndpointTest"
 docker-compose down # ensure we are fresh and clean.
 docker-compose rm -v
-docker-compose run web sh -c "mvn clean test -Dtest=FriendshipEndpointTest" 2>/dev/null | grep -v 'Download.* http'
+docker-compose run web sh -c "mvn clean test -Dtest=FriendshipEndpointTest"
 docker-compose down # ensure we are fresh and clean.
 docker-compose rm -v
-docker-compose run web sh -c "mvn clean test -Dtest=MeetingEndpointTest" 2>/dev/null | grep -v 'Download.* http'
+docker-compose run web sh -c "mvn clean test -Dtest=MeetingEndpointTest"
 docker-compose down # ensure we are fresh and clean.
 docker-compose rm -v
-docker-compose run web sh -c "mvn clean test -Dtest=MeetingUserEndpointTest" 2>/dev/null | grep -v 'Download.* http'
+docker-compose run web sh -c "mvn clean test -Dtest=MeetingUserEndpointTest"
 docker-compose down # ensure we are fresh and clean.
 docker-compose rm -v
-docker-compose run web sh -c "mvn clean test -Dtest=PlacesEndpointTest" 2>/dev/null | grep -v 'Download.* http'
+docker-compose run web sh -c "mvn clean test -Dtest=PlacesEndpointTest"
 docker-compose down # ensure we are fresh and clean.
 docker-compose rm -v
-docker-compose run web sh -c "mvn clean test -Dtest=UsersEndpointTest" 2>/dev/null | grep -v 'Download.* http'
-docker-compose down # ensure we are fresh and clean for next time
-
-
-docker-compose run web sh -c "mvn pmd:cpd-check && mvn pmd:check" | grep -v 'Download.* http'
+docker-compose run web sh -c "mvn clean test -Dtest=UsersEndpointTest"
+docker-compose down # ensure we are fresh and clean.
+docker-compose rm -v
