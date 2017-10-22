ca54a257bb743fa1c1fe0f0c03651b43118de4bb
James Stone <jamesmstone@users.noreply.github.com>
2017-10-22 16:49:45 +1100

    make tests less verbose while building, now doesnt show download progress of dependencies

--- tests.sh
+++ tests.sh
@@ -4,14 +4,15 @@ set -o pipefail
 
 runTest(){
 	docker-compose down # ensure we are fresh and clean.
-	docker-compose run -T web sh -c "$1" | tee out.txt || true; grep -q "BUILD SUCCESS" out.txt; rm out.txt
+	docker-compose run -T web sh -c "$1" | grep -v 'Download.* http' | tee out.txt || true; grep -q "BUILD SUCCESS" out.txt; rm out.txt
+
 	# ensure we are fresh and clean.
 	docker-compose rm -v
 	docker-compose down
 }
 
 docker-compose down # ensure we are fresh and clean.
-docker-compose build
+docker-compose build | grep -v 'Download.* http'
 runTest "mvn pmd:cpd-check && mvn pmd:check"
 runTest "mvn clean test -Dtest=ChatEndpointTest"
 runTest "mvn clean test -Dtest=FriendshipEndpointTest"
